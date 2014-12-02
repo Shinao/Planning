c9b5f6c5605b6b142c489e5c581aa7b4d577b0ad
unknown <rmonnerat.pro@gmail.com>
2014-12-02 12:53:36 +0100

    Repetition rules add dynamic

--- php/dialogRepetitionRules.php
+++ php/dialogRepetitionRules.php
@@ -1,11 +1,12 @@
 <script type="text/javascript">
+var	exceptions = [];
 $(document).ready(function () {
   $("#dialogRepetitionRules").dialog({
     autoOpen: false,
       modal: false,
       minWidth: 400,
       buttons: {
-	"Suivant": function() {
+	"Finir": function() {
 	  $.ajax(
 	    {
 	      type: "POST",
@@ -31,35 +32,22 @@ $(document).ready(function () {
 	  }
       },
 	close: function() {
-	    resetEvents();
 	}
   }).draggable();
 
-  // Setup events
-  $(".nameMember").each(function(index) { 
-    $(this).addClass('focusable');
-    $(this).on('click', function() {
-      $(this).toggleClass('focused');
-    });
-  });
-  $(".dayNumber").each(function(index) { 
-    $(this).addClass('focusable');
-    $(this).on('click', function() {
-      if ($(".dayNumber.focusable.focused").length > 0)
-	$(".dayNumber.focusable.focused").removeClass('focused');
-      $(this).addClass('focused');
-    });
-  });
-
+  $("#addException").on('click', function()
+{
+  var	ex = [$(".legendPlanningItem.focused").data('id'), $("#ex_day").val()];
+  exceptions.push(ex);
+  $(this).next().next().after("<span>" + $(".legendPlanningItem.focused").children().eq(0).html() + " - Jours " + ex[1] + "</span></br>");
+});
 });
 </script>
 
-<div id="dialogRepetitionRules" class="ui-widget" title="Action : Répétition">
+<div id="dialogRepetitionRules" class="ui-widget" title="Répétition : Exception">
 	<form id="frmRepetition">
-		<span>- Test2</span></br></br>
-		<span>- Selectionnez la date de depart</span></br></br>
-		<span>Intervalle (Jours) : <input type="number" name="repetition" id="repetition" min="1" max="9999" class="text ui-widget-content ui-corner-all" /></span></br></br>
-		<span>Durée : <input type="number" name="rep_month" id="rep_month" value="0" min="0" max="12" class="text ui-widget-content ui-corner-all" /> Mois 
-		<input type="number" value="0" name="rep_day" id="rep_day" min="0" max="31" class="text ui-widget-content ui-corner-all" /> Jours</span></br>
+		<span>Voulez-vous ajouter des exceptions à la répétition ?</span></br></br>
+		<span>- Selectionnez le type</span></br>
+		<span>Jours : <input type="number" name="ex_day" id="ex_day" value="0" min="-31" max="31" class="text ui-widget-content ui-corner-all" /> </span> <input style="float:right;" id="addException" type="button" value="Ajouter"/></br></br>
 	</form>
 </div>
